c25aa3818155301bb2c54a3f688c1bb2a8b33695
UghurGambarov <ugur08qemberov@gmail.com>
2026-04-08 07:12:32 +0000

    asdf

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if "{:c}" not in "qe":
+    if "{:c}" != "e" or "{:c}" != "q":
         print("{:c}".format(i), end="")
     else:
         continue
