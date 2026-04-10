e6306dd3bd5c822e79751ee9c6c7200891b0ac09
UghurGambarov <ugur08qemberov@gmail.com>
2026-04-10 12:31:40 +0000

    asdf

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,37 +1,28 @@
-Write a class Square that defines a square by: (based on 3-square.py)
-
-Private instance attribute: size:
-property def size(self): to retrieve it
-property setter def size(self, value): to set it:
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
-Instantiation with optional size: def __init__(self, size=0):
-Public instance method: def area(self): that returns the current square area
-You are not allowed to import any module
-Why?
-
-Why a getter and setter?
-
-Reminder: size is a private attribute. We did that to make sure we control the type and value. Getter and setter methods are not 100% Python, but more OOP. With them, you will be able to validate the assignment of a private attribute and also define how getting the attribute value will be available from outside - by copy? by assignment? etc. Also, adding type/value validation in the setter will centralize the logic, since you will do it in only one place.
-
-guillaume@ubuntu:~/$ cat 4-main.py
 #!/usr/bin/python3
-Square = __import__('4-square').Square
+"""This module defines a Square class with size property and area."""
 
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
 
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+class Square:
+    """This class defines a square with controlled size access."""
 
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
+    def __init__(self, size=0):
+        """Initialize the square with a validated size."""
+        self.size = size  # use setter
 
-guillaume@ubuntu:~/$ ./4-main.py
-Area: 7921 for size: 89
-Area: 9 for size: 3
-size must be an integer
-guillaume@ubuntu:~/$ 
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
