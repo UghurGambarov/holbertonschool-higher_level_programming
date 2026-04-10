b3811f76306104be1c4e1703130ddef82fa867b0
UghurGambarov <ugur08qemberov@gmail.com>
2026-04-10 11:45:56 +0000

    asdf

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -7,11 +7,3 @@ def no_c(my_string):
             new_string += char
 
     return new_string1~#!/usr/bin/python3
-def no_c(my_string):
-    new_string = ""
-
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-
-    return new_string
