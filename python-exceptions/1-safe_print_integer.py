dfb463ed310d4f225f66fbea99c0dc29e9ae5e1c
UghurGambarov <ugur08qemberov@gmail.com>
2026-04-16 14:47:40 +0000

    asdf

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        if int(value) == value:
+            print("{:d}".format(value))
     except:
         print("{} is not an integer".format(value))
