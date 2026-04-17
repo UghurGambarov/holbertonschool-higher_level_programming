8e0ea8e62bf3742f541f2c40916a29be86f95ce2
UghurGambarov <ugur08qemberov@gmail.com>
2026-04-17 08:56:29 +0000

    asdf

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,2 +1,6 @@
 -- ads
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
