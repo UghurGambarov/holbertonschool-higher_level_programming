3decf79161b87b73e9f3543e64913859de1f7e6d
UghurGambarov <ugur08qemberov@gmail.com>
2026-04-18 16:16:07 +0000

    asdf

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-res=$(curl -s -o /tmp/body -w "%{http_code}" "$1")
-[ "$res" -eq 200 ] && cat /tmp/body
+# aklsdfkalsdfalkj
+res=$(curl -s -o /tmp/body -w "%{http_code}" "$1") [ "$res" -eq 200 ] && cat /tmp/body
