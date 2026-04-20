834de13600ae704aa193e3da7cb5721238970c35
Ughur Gambarov <122470324+UghurGambarov@users.noreply.github.com>
2026-04-20 09:47:43 +0400

    Update 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# aklsdfkalsdfalkj
-res=$(curl -s -o /tmp/body -w "%{http_code}" "$1") [ "$res" -eq 200 ] && cat /tmp/body
+# Sends a GET request to a URL and displays the body only if the response status is 200
+curl -s -w "%{http_code}" "$1" | sed -e '/200$/!d' -e 's/[0-9]\{3\}$//'
