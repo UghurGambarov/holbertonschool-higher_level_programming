ebf62c02ecd8fc0cf6b0b5105a8e9307fb6c8b75
Ughur Gambarov <122470324+UghurGambarov@users.noreply.github.com>
2026-04-20 09:49:11 +0400

    Modify GET request to save body to temp file

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body only if the response status is 200
-curl -s -w "%{http_code}" "$1" | sed -e '/200$/!d' -e 's/[0-9]\{3\}$//'
+curl -s -o /tmp/body -w "%{http_code}" "$1" | grep -q "200" && cat /tmp/body
