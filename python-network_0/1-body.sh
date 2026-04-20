353d76a0635992980163eb761267d2860c92fbf2
UghurGambarov <ugur08qemberov@gmail.com>
2026-04-20 05:53:40 +0000

    asdf

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body only if the response status is 200
-curl -s -o /tmp/body -w "%{http_code}" "$1" | grep -q "200" && cat /tmp/body
+curl -sL "$1" -w "%{http_code}" | sed -e '$!b' -e 's/200$//' -e 't' -e 'd'
