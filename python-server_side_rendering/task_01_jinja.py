f9325f9371cf67126ca60d8bd02c191e8e33cde8
Ughur Gambarov <122470324+UghurGambarov@users.noreply.github.com>
2026-04-22 17:30:06 +0400

    Update task_01_jinja.py

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import json
 
 app = Flask(__name__)
 
@@ -14,18 +13,6 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-@app.route('/items')
-def items():
-    try:
-        with open('items.json', 'r') as file:
-            data = json.load(file)
-            items_list = data.get("items", [])
-    except Exception as e:
-        print(f"Error reading JSON: {e}")
-        items_list = []
-
-    return render_template('items.html', items=items_list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
